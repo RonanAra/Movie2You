0d481301362680802b436782b20f47f3b7cfc79c
ronanlm10@hotmail.com <ronanlm10@hotmail.com>
2024-07-21 21:43:29 -0300

    refactor: moved to presentation package

--- app/src/main/java/br/com/movie2you/presentation/MainActivity.kt
+++ app/src/main/java/br/com/movie2you/presentation/MainActivity.kt
@@ -1,16 +1,19 @@
-package br.com.movie2you
+package br.com.movie2you.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import br.com.movie2you.ui.theme.Movie2YouTheme
+import br.com.movie2you.presentation.splash.SplashScreen
+import br.com.movie2you.presentation.ui.theme.Movie2YouTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Movie2YouTheme {}
+            Movie2YouTheme {
+                SplashScreen()
+            }
         }
     }
 }
