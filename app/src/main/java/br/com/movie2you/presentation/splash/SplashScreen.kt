b5a19db8da83267ebfc3404e98a560574ec1ae3f
ronanlm10@hotmail.com <ronanlm10@hotmail.com>
2024-07-21 21:44:46 -0300

    feat: Impl splash screen with animation

---
+++ app/src/main/java/br/com/movie2you/presentation/splash/SplashScreen.kt
@@ -0,0 +1,34 @@
+package br.com.movie2you.presentation.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import br.com.movie2you.R
+import br.com.movie2you.presentation.ui.theme.Onyx
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun SplashScreen(onAnimationFinished: () -> Unit = {}) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.splash_animation)
+    )
+    val progress by animateLottieCompositionAsState(composition = composition)
+
+    LaunchedEffect(key1 = progress) {
+        if (progress >= 1) onAnimationFinished.invoke()
+    }
+
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Onyx)
+    )
+}
